74be9855daa77570f6c61578d027862ead5a9587
ingan <isabelngan27@gmail.com>
2019-08-08 16:17:51 -0700

    updating projects and work from resume

--- js/ingan.js
+++ js/ingan.js
@@ -2,20 +2,30 @@ var forMore = $("<div>")
 forMore.addClass("forMore bounce-1 helloScroll");
 forMore.text("click to play");
 
-$("#container-2-sec").on("mouseover", ".work", function () {
+$("#container-2-sec").on("mouseover", ".game", function () {
     var game = $(this).attr('id');
     $(".subTitle" + game).show();
     $(".worktitle" + game).show();
     $("#" + game).prepend(forMore);
-}).on("mouseout", ".work", function () {
+}).on("mouseout", ".game", function () {
     var game = $(this).attr('id');
     $(".subTitle" + game).hide();
     $(".worktitle" + game).hide();
     $(forMore).detach();
 });
 
-$("#container-3-sec").on("mouseover", "#bWokeDIV", function() {
+$("#bwokelogo").hide();
+$("#container-2-sec").on("mouseover", "#bWokeDIV", function() {
     $(".morewoke").show();
+    $(".worktitleBWoke").show();
+    $(".subTitlebWoke").show();
+    $(".bwokeDesc").show();
+    $("#bwokelogo").show();
 }).on("mouseout", function () {
     $(".morewoke").hide();
+    $(".morewoke").hide();
+    $(".worktitleBWoke").hide();
+    $(".subTitlebWoke").hide();
+    $(".bwokeDesc").hide();
+    $("#bwokelogo").hide();
 });
